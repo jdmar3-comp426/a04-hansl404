e64359267a4a523a8704495e7d89e025cebc6ccd
han5010 <error404@live.unc.edu>
2021-11-23 10:45:59 -0500

    sixth try

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.get("/app/user/:id", (req, res) => {
 
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req,res) => {
-	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user) pass = COALESCE(?,pass) WHERE id=?")
+	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id=?")
 	const to_update = stmt.run(req.body.user, md5(req.body.pass), req.params.id)
 	res.status(200).json({"message": to_update.changes + " record updated: ID " + req.params.id + " (200)"})
 })
